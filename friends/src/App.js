9bdc94d10f3e0ff0e597c2d707bc8af370d7489f
tygedavis <tyge.davis@gmail.com>
2019-12-17 16:13:25 -0700

    Friends now render on the screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
 //components
@@ -11,6 +11,10 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <div>
+          <Link to='/login'>Login</Link>
+          <Link to='friendslist'>Friends</Link>
+        </div>
         <Switch>
           <PrivateRoute path="/friendslist" component={FriendsList} />
           <Route path="/login" component={Login} />
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
+import Friend from './Friend';
+
 class FriendsList extends React.Component {
+  state = {
+    friends : []
+  }
+  
+  componentDidMount() {
+    this.getFriends();
+  }
+
+  getFriends = () => {
+    axiosWithAuth()
+      .get("/friends")
+      .then(res => {
+        //console.log('Friends Data', res)
+        this.setState({
+          friends: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
+    //console.log('Friends State', this.state.friends)
     return(
       <div>
-        <h1>This is a Test from FriendsList</h1>
+        <h1>Friends List</h1>
+        {this.state.friends.map(item => <Friend item={item} />)}
       </div>
     )
   };
